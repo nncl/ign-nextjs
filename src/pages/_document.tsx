d9740d55a9371df152867476432eadd496b8ebd6
Caue <nncl@live.com>
2021-06-01 18:44:44 -0300

    feat(pages): Creating _document and adding custom font family

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+/*
+ * Loaded once, different than the _app.
+ */
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+                rel="stylesheet"/>
+
+          <title>ig.news</title>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+    )
+  }
+}
