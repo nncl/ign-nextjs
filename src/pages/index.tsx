49aaafe0fa3af531e691a6057136e05e1a5d5721
Caue <nncl@live.com>
2021-06-15 18:03:48 -0300

    feat(components): Creating Subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -12,11 +13,15 @@ export default function Home() {
       <main className={ styles.contentContainer }>
         <section className={ styles.hero }>
           <span>👏 Hey, welcome</span>
+
           <h1>News about the <span>React</span> world.</h1>
+
           <p>
             Get access to all the publications <br/>
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding"/>
