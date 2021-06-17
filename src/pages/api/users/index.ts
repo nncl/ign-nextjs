1af7c3824ee9b8f891fae85a31d75c1f25056cf0
Caue <nncl@live.com>
2021-06-17 16:29:22 -0300

    refactor(api): removing users' directory

--- src/pages/api/users/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const users = [
-    { id: 1, name: 'Caue' }
-  ]
-
-  return res.json(users)
-}
