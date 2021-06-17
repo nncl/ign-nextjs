5d1d2dcaf51c822d57a03bea759b7ef0a11c6233
Caue <nncl@live.com>
2021-06-17 16:30:34 -0300

    feat(components): Signing in/out with Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,23 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [ session, loading ] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type='button' className={ styles.signButton }>
+  return session ? (
+    <button type='button' className={ styles.signButton } onClick={ () => signOut() }>
       <FaGithub color='#04d361'/>
-      Cauê Almeida
-      <FiX color='#737380' className={styles.closeIcon} />
+      { session?.user?.name }
+      <FiX color='#737380' className={ styles.closeIcon }/>
     </button>
   ) : (
-    <button type='button' className={ styles.signButton }>
+    <button type='button' className={ styles.signButton } onClick={ () => signIn('github') }>
       <FaGithub color='#eba417'/>
-      Sign in with Github
+      {loading ? 'Loading...' : 'Sign in with Github'}
     </button>
   )
 
