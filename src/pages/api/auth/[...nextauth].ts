3cada8b22ac63cc92ce1b41c598cae93c708bf1c
Caue <nncl@live.com>
2021-06-18 18:12:58 -0300

    feat(services): Adding callback to store user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
+  debug: process.env.NODE_ENV === 'development',
+
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
@@ -9,5 +14,27 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  debug: process.env.NODE_ENV === 'development'
+
+  jwt: {
+    signingKey: process.env.SINGIN_KEY
+  },
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
