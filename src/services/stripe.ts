b707d85ec37f74c0f314b18b702f7648bd87cadb
Caue <nncl@live.com>
2021-06-18 13:56:46 -0300

    refactor(services): Getting package information

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from 'stripe'
-import { version } from '../../package.json'
+import info  from '../../package.json'
+
+const { version } = info;
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
