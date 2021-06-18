f61167001c173181f78fbf816cdeb1166f6725a7
Caue <nncl@live.com>
2021-06-18 17:40:22 -0300

    feat(seo): Setting required meta tags and app's viewport width

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { useRouter } from 'next/router';
 import { Provider as NextAuthProvider } from 'next-auth/client'
 
 import { Header } from '../components/Header';
@@ -6,9 +8,18 @@ import { Header } from '../components/Header';
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const site = "https://test.com";
+  const canonicalURL = site + useRouter().asPath;
+
   return (
     <NextAuthProvider session={ pageProps.session }>
+      <Head>
+        <link rel='canonical' href={canonicalURL}/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
+
       <Header/>
+
       <Component { ...pageProps } />
     </NextAuthProvider>
   )
