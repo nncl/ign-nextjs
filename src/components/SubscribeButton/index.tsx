2b7aee849efbfbe237580d6ea4fcac121492676e
Caue <nncl@live.com>
2021-07-07 20:45:43 -0300

    feat(components): Redirecting user to checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
 import { signIn, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -9,14 +11,25 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [ session ] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return
     }
 
-    // Creating Stripe Checkout Session
-    // TODO
+    /*
+     * Creating Stripe Checkout Session
+     */
+    try {
+      const { data } = await api.post('/subscribe')
+      const { sessionId } = data
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+
+    } catch ({ message }) {
+      alert(message)
+    }
   }
 
   return (
