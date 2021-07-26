1226fa12882e132fd77f91d86f1c75e472310127
Caue <nncl@live.com>
2021-07-26 20:11:20 -0300

    feat: Storing subscription on FaunaDB

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,50 @@
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+) {
+
+  /*
+   * Search on FaunaDB an user with ID equals to {customerId}
+   */
+
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  /*
+   * Search all subscription data
+   */
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+  /*
+   * Save subscription data on FaunaDB
+   */
+
+  const subscriptionData = {
+    id: subscription.id,
+    user_id: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
+
+}
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
 import { Stripe } from 'stripe';
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
   const chunks = []
@@ -46,7 +47,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event
 
     if (relevantEvents.has(type)) {
-      console.log('Event received', event); // FIXME
+
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+            )
+
+            break;
+          default:
+            throw new Error('Unhandled event');
+        }
+      } catch (e) {
+        // Sentry, bugsnag...
+        return res.json({ error: 'Webhook handler failed' });
+      }
+
     }
 
     res.status(200).json({ received: true })
