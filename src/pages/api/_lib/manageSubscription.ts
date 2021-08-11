e7f7cbe1f639a02102e0ed89fae831b5898070ec
Caue <nncl@live.com>
2021-08-11 20:54:47 -0300

    feat: Updating Subscription
    We listen to Stripe customer subscription events and depending on which type we upsert it on our database.

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,6 +5,7 @@ import { stripe } from '../../../services/stripe';
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false
 ) {
 
   /*
@@ -40,11 +41,26 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    q.Replace(
+      q.Select(
+        'ref',
+        q.Get(
+          q.Match(
+            q.Index('subscription_by_id'),
+            subscriptionId
+          )
+        )
+      ),
+      { data: subscriptionData }
+    );
+  }
 
 }
