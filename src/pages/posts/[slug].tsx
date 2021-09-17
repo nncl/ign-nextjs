652e732ce5b37e566ce0e1ddc94d4ff48514363a
Caue <nncl@live.com>
2021-09-16 21:06:27 -0300

    feat(api/auth): Redirecting user to home if not subscribed

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -5,6 +5,7 @@ import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './post.module.scss';
+import { getSession } from 'next-auth/client';
 
 interface PostProps {
   post: {
@@ -35,7 +36,17 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
   const { slug } = params;
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
   const response = await prismic.getByUID('post', String(slug), {});
